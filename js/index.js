01e6a3701a33e266640a506290b962222ffca3ff
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-03-04 15:39:17 -0300

    Desktop version

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ const navList = document.querySelector('#nav-list');
 document.querySelector('.toggle-nav').addEventListener('click', () => {
    let hidden = navList.getAttribute("data-hidden");
 
-   navList.setAttribute('data-hidden', !(hidden == "true"));
+   navList.setAttribute('data-hidden', hidden == "false");
 })
