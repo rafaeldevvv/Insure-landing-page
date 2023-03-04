06f585364e565afbcadcd443740867b3c3d89153
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-03-04 16:24:50 -0300

    .

--- js/index.js
+++ js/index.js
@@ -4,4 +4,4 @@ document.querySelector('.toggle-nav').addEventListener('click', () => {
    let hidden = navList.getAttribute("data-hidden");
 
    navList.setAttribute('data-hidden', hidden == "false");
-})
+});
